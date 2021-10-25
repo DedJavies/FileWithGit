e01dec92edc2863b3eecb7e9c820f1bf5c326bc8
Jed Davies <270102467@yoobeestudent.ac.nz>
2021-10-26 09:23:47 +1300

    first commit from mem1, jd2

---
+++ FilesWithGit/FilesWithGit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+struct Shoes {
+	string brand;
+	string material;
+	string type;
+	string colour;
+	string size;
+	float price;
+
+	Shoes(string b, string m, string t, string c, string s, float p) {
+		brand = b;
+		material = m;
+		type = t;
+		colour = c;
+		size = s;
+		price = p;
+	}
+};
+
+int main()
+{
+
+}
